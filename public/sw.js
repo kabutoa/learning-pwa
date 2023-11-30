e3cb887424d00e9d44c025ee12169e04bb77c76e
kabutoa <yinyun957@163.com>
2023-11-30 17:44:23 +0800

    style: 添加代码注释

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "cache_v1";
 const URLS = [
-  "/",
+  "/", // => 指向index.html
   "/index.css",
   "/manifest.json",
   "/images/search.png",
@@ -14,9 +14,11 @@ const URLS = [
   "/lib/axios.js",
   "/api/articles",
 ];
+
 // 缓存优先
 const cacheFirst = async (req) => {
   const cache = await caches.open(CACHE_NAME);
+  // 查询缓存
   const cached = await cache.match(req);
   if (cached) {
     return cached;
@@ -24,11 +26,13 @@ const cacheFirst = async (req) => {
   const fresh = await fetch(req);
   return fresh;
 };
+
 // 网络优先
 const networkFirst = async (req) => {
   const cache = await caches.open(CACHE_NAME);
   try {
     const fresh = await fetch(req);
+    // 必须创建副本
     cache.put(req, fresh.clone());
     return fresh;
   } catch {
@@ -37,30 +41,38 @@ const networkFirst = async (req) => {
   }
 };
 
-// 缓存
+// 添加缓存
 self.addEventListener("install", async (event) => {
   const cache = await caches.open(CACHE_NAME);
   await cache.addAll(URLS);
+  // 触发activate事件
   await self.skipWaiting();
 });
-
+// 删除旧缓存
 self.addEventListener("activate", async () => {
   const keys = await caches.keys();
-  keys.forEach((key) => {
-    if (key !== CACHE_NAME) {
-      caches.delete(key);
-    }
-  });
+  await Promise.all(
+    keys.map((key) => {
+      if (key !== CACHE_NAME) {
+        // 返回 promise
+        return caches.delete(key);
+      }
+    })
+  );
+  // 使service worker立即生效
   await self.clients.claim();
 });
-
+// 处理缓存
 self.addEventListener("fetch", async (e) => {
   const req = e.request;
   const url = new URL(req.url);
+  // 只缓存同源资源
   if (url.origin !== self.origin) {
     return;
   }
+  // 接口资源走网络优先
   if (req.url.includes("/api")) {
+    // 结果返回给浏览器
     e.respondWith(networkFirst(req));
   } else {
     e.respondWith(cacheFirst(req));
